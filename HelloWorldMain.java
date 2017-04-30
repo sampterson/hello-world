89c0f2fa8c64194939e03725104c4cedd45dcd42
sampterson <peterson.sam.j12@gmail.com>
2017-04-30 13:39:05 -0500

    Add files via upload

--- HelloWorldMain.java
+++ HelloWorldMain.java
@@ -3,12 +3,16 @@ package com.whatupstudio.helloworld.main;
 public class HelloWorldMain {
 
 	public static void main(String[] args) {
+		
+		//I am adding some comments for readability
 		System.out.println("Hello, World!");
 		int x = 0;
 		for (int i = 0; i < 10; i++){
 			System.out.println(x);
 			x++;
 		}
+		
+		System.out.println("Another print!");
 	}
 
 }
